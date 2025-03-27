b22802e4cdd10d7aa5cd93bc8a7f2c558d4f4c48
Gabriel Kis <gabriel.kis@planeta.inf.br>
2025-03-27 17:31:18 -0300

    Added driver layer to Blue LED on Board

--- src/main.c
+++ src/main.c
@@ -4,20 +4,43 @@
  */
 
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/sensor.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
 #include <app_version.h>
 
+/* LED device tree specification */
+#define LED0_NODE DT_ALIAS(led0)
+
+#if !DT_NODE_HAS_STATUS(LED0_NODE, okay)
+#error "LED0 GPIO node is not ready"
+#endif
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
 int main(void)
 {
-	printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
-	printk("Use the sensor to change LED blinking period\n");
-	uint32_t count=0;
+    int ret;
 
-	while (1) {
-		printk("count: %d\n", count++);
-		k_sleep(K_MSEC(1000));
-	}
+    printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
 
-	return 0;
+    if (!device_is_ready(led.port)) {
+        printk("Error: LED device %s is not ready\n", led.port->name);
+        return 1;
+    }
+
+    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    if (ret < 0) {
+        printk("Error %d: failed to configure LED pin\n", ret);
+        return 1;
+    }
+
+    while (1) {
+        /* Toggle LED state */
+        gpio_pin_toggle_dt(&led);
+        k_sleep(K_MSEC(50));
+        gpio_pin_toggle_dt(&led);
+        k_sleep(K_MSEC(1000));
+    }
+
+    return 0;
 }
